a7c56e50196f0b27fe48aa46eb851dfb9dba827f
Brian Scholer <1260690+briantist@users.noreply.github.com>
2022-04-15 18:59:11 -0400

    add service command

--- src/CcgVault/Program.cs
+++ src/CcgVault/Program.cs
@@ -1,10 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using COMAdmin;
 using Fclp;
+using System;
+using System.Diagnostics;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace CcgVault
 {
@@ -66,6 +67,76 @@ namespace CcgVault
             }
         }
 
+        class CliCommandService : ICliCommand
+        {
+            public bool Install { get; set; }
+            public bool Uninstall { get; set; }
+
+            public void Help(string text)
+            {
+                Console.WriteLine("ccgvault.exe service");
+                Console.WriteLine();
+                Console.WriteLine("Installs or uninstalls a Windows (NT) service to associate with the COM+ application.");
+                Console.WriteLine();
+                Console.WriteLine("The following options are accepted:");
+                Console.WriteLine(text);
+                Exit(-1);
+            }
+
+            public void Invoke()
+            {
+                if (Install && Uninstall)
+                {
+                    Console.WriteLine("Both --install and --uninstall were specified. Choose one or the other.");
+                    Exit(-1);
+                }
+
+                if (!(Install || Uninstall))
+                {
+                    Console.WriteLine("Neither --install nor --uninstall were specified. Choose one or the other.");
+                    Exit(-1);
+                }
+
+                var appId = Assembly.GetExecutingAssembly().GetCustomAttribute<ApplicationIDAttribute>().Value;
+                var serviceName = Assembly.GetExecutingAssembly().GetCustomAttribute<ApplicationNameAttribute>().Value;
+
+                // https://docs.microsoft.com/en-us/windows/win32/api/comadmin/nf-comadmin-icomadmincatalog2-createserviceforapplication
+                var oCatalog = (ICOMAdminCatalog2)Activator.CreateInstance(Type.GetTypeFromProgID("ComAdmin.COMAdminCatalog"));
+
+                if (Install)
+                {
+                    try
+                    {
+                        oCatalog.CreateServiceForApplication(
+                            bstrApplicationIDOrName: appId.ToString("B"), // Using an AppID, the GUID needs to be formatted with curly braces {}
+                            bstrServiceName: serviceName,
+                            bstrStartType: "SERVICE_DEMAND_START",
+                            bstrErrorControl: "SERVICE_ERROR_NORMAL",
+                            bstrDependencies: null,
+                            bstrRunAs: null,
+                            bstrPassword: null,
+                            bDesktopOk: false);
+                    }
+                    catch (COMException e) when (e.HResult == -2147023823) // 0x80070431 ERROR_SERVICE_EXISTS
+                    {
+                        // ignore
+                    }
+                }
+
+                if (Uninstall)
+                { 
+                    try
+                    {
+                        oCatalog.DeleteServiceForApplication(bstrApplicationIDOrName: appId.ToString("B")); // Using an AppID, the GUID needs to be formatted with curly braces {}
+                    }
+                    catch (COMException e) when (e.HResult == -2146368458) // 0x80110436 COMADMIN_E_SERVICENOTINSTALLED
+                    {
+                        // ignore
+                    }
+                }
+            }
+        }
+
         public static void Main(string[] args)
         {
             string cmd = "";
@@ -105,9 +176,26 @@ namespace CcgVault
 
                     result = _ping.Parse(arguments);
                     command = _ping.Object;
-
+                    
                     break;
 
+                case "service":
+                    var _service = new FluentCommandLineParser<CliCommandService>();
+
+                    _service.SetupHelp("?", "help")
+                        .Callback(text => _service.Object.Help(text));
+
+                    _service.Setup<bool>(arg => arg.Install)
+                        .As('i', "install");
+
+                    _service.Setup<bool>(arg => arg.Uninstall)
+                        .As('u', "uninstall");
+
+                    result = _service.Parse(arguments);
+                    command = _service.Object;
+
+                    break;
+                        
                 case "help":
                 case "":
                 default:
