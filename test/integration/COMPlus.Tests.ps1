1bcb8467e2140551f879b3825d0a1c646a658839
Brian Scholer <1260690+briantist@users.noreply.github.com>
2022-04-19 22:44:26 -0400

    add COM+ tests (#6)
    * add COM+ tests
    
    * add some COM+ property tests
    
    * add COM+ tests to workflow

---
+++ test/integration/COMPlus.Tests.ps1
@@ -0,0 +1,87 @@
+#Requires -Module @{ ModuleName = 'Pester'; ModuleVersion = '5.2.0' }
+
+BeforeAll {
+    $data = Import-PowerShellDataFile -LiteralPath $PSScriptRoot\TestData.psd1
+
+    $appId = [Guid]::Parse($data.ComPlus.ApplicationID)
+    $ccgPluginId = [Guid]::Parse($data.ComPlus.CcgPlugin.ID)
+
+    $cat = New-Object -ComObject ComAdmin.ComAdminCatalog
+    $apps = $cat.GetCollection('Applications')
+    $apps.Populate()
+
+    $app =  $apps | Where-Object -FilterScript {
+        $appId -eq $_.Key
+    }
+
+    if ($app) {
+        $appProps = $apps.GetCollection('PropertyInfo', $app.Key)
+        $appProps.Populate()
+
+        $appInfo = @{}
+        $appProps | ForEach-Object -Process {
+            try {
+                $appInfo[$_.Name] = $app.Value($_.Name)
+            }
+            catch {}
+        }
+
+        $components = $apps.GetCollection('Components', $app.Key)
+        $components.Populate()
+
+        $ccgPlugin = $components | Where-Object -FilterScript {
+            $ccgPluginId -eq $_.Key
+        }
+
+        if ($ccgPlugin) {
+            $compProps = $components.GetCollection('PropertyInfo', $ccgPlugin.Key)
+            $compProps.Populate()
+
+            $compInfo = @{}
+            $compProps | ForEach-Object -Process {
+                try {
+                    $compInfo[$_.Name] = $ccgPlugin.Value($_.Name)
+                }
+                catch {}
+            }
+        }
+    }
+}
+
+Describe 'COM+ tests' {
+    Context 'Present' -Tag Present {
+        It 'There is exactly 1 app' {
+            $app.Count | Should -BeExactly 1
+        }
+
+        It 'App name is CcgVault' {
+            $app.Name | Should -BeExactly CcgVault
+        }
+
+        It 'App identity is <data.ComPlus.Identity>' {
+            $appInfo.Identity | Should -Be $data.ComPlus.Identity
+        }
+
+        It 'App is enabled' {
+            $appInfo.IsEnabled | Should -BeTrue
+        }
+
+        It 'App has exactly 1 component' {
+            $ccgPlugin.Count | Should -BeExactly 1
+        }
+
+        It 'Component name is <data.ComPlus.CcgPlugin.Name>' {
+            $ccgPlugin.Name | Should -BeExactly $data.ComPlus.CcgPlugin.Name
+        }
+
+        It 'Component is enabled' {
+            $compInfo.IsEnabled | Should -BeTrue
+        }
+    }
+
+    Context 'Absent' -Tag Absent {
+        It 'App does not exist' {
+            $app | Should -BeNullOrEmpty
+        }
+    }
+}
--- test/integration/TestData.psd1
+++ test/integration/TestData.psd1
@@ -1,6 +1,10 @@
 @{
     ComPlus = @{
         ApplicationID = '{2D79156B-1DE7-4A29-9428-7B7A591B176E}'
+        CcgPlugin = @{
+            ID = '{01BF101D-BFB6-433F-B416-02885CDC5AD3}'
+            Name = 'CcgCredProvider'
+        }
         Identity = 'NT AUTHORITY\LocalService'
     }
 
